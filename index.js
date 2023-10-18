804bca5f5793c329bd5346d2fa2923a1d68809f0
momo2396 <foyazunnesamomo.345@gmail.com>
2023-10-19 01:36:46 +0600

    get operation completed

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) =>{
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kxy8ozq.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -24,24 +24,29 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    
+    const productCollection = client.db('productDB').collection('product');
+
+    app.get('/product/:brand', async (req, res)=>{
+        const myBrand = req.params.brand;
+        const cursor = productCollection.find({
+            'brand': myBrand,
+        });
+        const result = await cursor.toArray();
+        res.send(result);
+    } )
     app.post('/product', async(req, res)=>{
         const newProduct = req.body;
         console.log(newProduct);
+        const result = await productCollection.insertOne(newProduct);
+        res.send(result);
     })
-
-
-
-
-
-
+    
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-    // Ensures that the client will close when you finish/error
+  
     // await client.close();
   }
 }
